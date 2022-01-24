650c08323adeec63c25b4d8c2e9dcab059c34126
Devi Acharya <dacharya64@gmail.com>
2022-01-24 10:37:37 -0800

    removing old registration code

--- flaskblog/routes.py
+++ flaskblog/routes.py
@@ -1,6 +1,5 @@
 from flask import render_template, url_for, flash, redirect
 from flaskblog import app
-from flaskblog.forms import RegistrationForm, LoginForm
 from flaskblog.models import User, Post
 
 
@@ -9,6 +8,10 @@ posts = [
     {
         "tag": "sadies_sob_story",
         "name": "Sadie's Sob Story"
+    },
+     {
+        "tag": "fullers_electrical_repair",
+        "name": "Fuller's Electrical Repair"
     }
 ]
 
@@ -23,26 +26,6 @@ def home():
 def about():
     return render_template('about.html', title='About')
 
-
-@app.route("/register", methods=['GET', 'POST'])
-def register():
-    form = RegistrationForm()
-    if form.validate_on_submit():
-        flash(f'Account created for {form.username.data}!', 'success')
-        return redirect(url_for('home'))
-    return render_template('register.html', title='Register', form=form)
-
-
-@app.route("/login", methods=['GET', 'POST'])
-def login():
-    form = LoginForm()
-    if form.validate_on_submit():
-        if form.email.data == 'admin@blog.com' and form.password.data == 'password':
-            flash('You have been logged in!', 'success')
-            return redirect(url_for('home'))
-        else:
-            flash('Login Unsuccessful. Please check username and password', 'danger')
-    return render_template('login.html', title='Login', form=form)
 
  # "player_visited": false,
  #        "lead-outs": [
